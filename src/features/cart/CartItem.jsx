180de4842af828b181bf48ddb4e67c66c8af212c
Niamask <Niamaskay@gmail.com>
2024-04-27 19:37:06 +0100

    Deleting Cart items

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,6 +1,5 @@
-import Button from '../../ui/Button';
+import DeleteItem from './DeleteItem';
 import { formatCurrency } from '../../utils/helpers';
-
 import PropTypes from 'prop-types';
 
 function CartItem({ item }) {
@@ -9,11 +8,11 @@ function CartItem({ item }) {
   return (
     <li className="sm py-3 sm:flex sm:items-center sm:justify-between">
       <p className="mb-1  sm:mb-0">
-        {quantity}&times; {name} {pizzaId}
+        {quantity}&times; {name}
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-        <Button type="small">Delete</Button>
+        <DeleteItem pizzaId={pizzaId} />
       </div>
     </li>
   );
